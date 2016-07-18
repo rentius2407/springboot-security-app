765736b6e12ad3541ff80c59e4004679b664f3ab
Rentius Engelbrecht <rentiusengelbrecht@capitecbank.co.za>
2016-07-18 13:13:02 +0200

    add functionality to package the jar as a war and deploy on Tomcat 8

--- src/main/java/com/ren/Application.java
+++ src/main/java/com/ren/Application.java
@@ -12,3 +12,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 }
+//@SpringBootApplication
+//public class Application extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        SpringApplication.run(Application.class, args);
+//    }
+//
+//}
