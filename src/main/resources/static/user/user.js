94d65c0d189e740bd2017089659a7de84192e0ea
rentius2407 <rentius2407dev@gmail.com>
2016-07-23 12:39:25 +0200

    load script when partial changes

--- src/main/resources/static/user/user.js
+++ src/main/resources/static/user/user.js
@@ -1,4 +1,9 @@
 var userCtrl = (function () {
+    $("#userSubmit").click(function () {
+        alert("Handler for .click() called.");
+        $('#nameLabel').text('Rentius');
+    });
+
     return {
         sayHello: function () {
             $('#nameLabel').text('Rentius');
