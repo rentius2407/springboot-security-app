d7159e480cd8da22bc5df5fb265c80619a6eb826
rentius2407 <rentius2407dev@gmail.com>
2016-07-28 17:31:42 +0200

    sequence on the property entity

--- src/main/java/com/ren/property/Property.java
+++ src/main/java/com/ren/property/Property.java
@@ -9,8 +9,8 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -18,7 +18,8 @@ import javax.persistence.Table;
 public class Property implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "propertySeq")
+    @SequenceGenerator(name = "propertySeq", sequenceName = "property_seq", allocationSize = 1)
     private long id;
     @Column(name = "name")
     private String name;
