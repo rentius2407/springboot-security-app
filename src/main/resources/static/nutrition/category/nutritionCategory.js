f5b72157b77f94885902d9d194121928731d4df0
rentius2407 <rentius2407dev@gmail.com>
2016-08-01 22:21:55 +0200

    add nutrition category options

--- src/main/resources/static/nutrition/category/nutritionCategory.js
+++ src/main/resources/static/nutrition/category/nutritionCategory.js
@@ -3,12 +3,12 @@ angular.module('app.nutrition.category', [])
 
             $stateProvider
                     .state('app.nutrition.category', {
-                        url: '/nutrition/category',
-//                        data: {
-//                            requireLogin: true,
-//                            moduleName: 'home',
-//                            moduleDisplayHeader: 'Home'
-//                        },
+                        url: '/category/:catName',
+                        resolve: {
+                            catName: ['$stateParams', function ($stateParams) {
+                                    return $stateParams.catName;
+                                }]
+                        },
                         views: {
                             'main@': {
                                 controller: 'NutritionCategoryController as nutritionCatCtrl',
@@ -17,9 +17,36 @@ angular.module('app.nutrition.category', [])
                         }
                     });
         })
-        .controller('NutritionCategoryController', function (UserDetailService) {
-            
+        .controller('NutritionCategoryController', function (UserDetailService, catName) {
             var nutritionCatCtrl = this;
+            nutritionCatCtrl.header = catName;
+            nutritionCatCtrl.addOption = false;
+
+            nutritionCatCtrl.options = [
+                {id: 1, name: 'Option1',
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.'
+                },
+                {id: 2, name: 'Option2',
+                    content: 'This is the new content that is being displayed'
+                }
+            ];
+
+            nutritionCatCtrl.addCategoryOption = function () {
+                if (nutritionCatCtrl.addOption) { //adding new at this moment
+
+                    var newOption = {
+                        name: nutritionCatCtrl.optionName,
+                        content: nutritionCatCtrl.optionContent
+                    };
+
+                    nutritionCatCtrl.options.push(newOption);
+                    nutritionCatCtrl.addOption = false;
+                } else {
+                    nutritionCatCtrl.optionName = '';
+                    nutritionCatCtrl.optionContent = '';
+                    nutritionCatCtrl.addOption = true;
+                }
+            };
         })
         .factory('NutritionCategoryService', function ($window) {
         });
